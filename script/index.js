689b782766941f873657fff8ce78bde334150bdf
reimondo <reimondo.zero@gmail.com>
2014-09-04 21:39:56 +0800

    add cookie

--- script/index.js
+++ script/index.js
@@ -2,7 +2,161 @@
  * Hehe, serious not want to add comment. Let it go.
  * Synchronous. 
  */
- 
+
+/* $begin cookie object construction */
+
+/* CookieUtil Oject -  */
+var CookieUtil = {
+
+    get: function (name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null;
+
+        if (cookieStart > -1) {
+            var cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart
+                + cookieName.length, cookieEnd));
+        }
+
+        return cookieValue;
+    },
+
+    set: function (name, value, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=" +
+                     encodeURIComponent(value);
+
+        if (expires instanceof Date) {
+            cookieText += "; expires=" + expires.toGMTString();
+        }
+
+        if (path) {
+            cookieText += "; path=" + path;
+        }
+
+        if (domain) {
+            cookieText += "; domain=" + domain;
+        }
+
+        if (secure) {
+            cookieText += "; secure";
+        }
+
+        document.cookie = cookieText;
+    },
+
+    unset: function (name, path, domain, secure) {
+        this.set(name, "", new Date(0), path, domain, secure);
+    }
+};
+
+/* subCookieUtil object
+ * - document.cookie=data=name=xxx&phone=123456
+ */
+var SubCookieUtil = {
+    get: function (name, subName) {
+        var subCookies = this.getAll(name);
+        if (subCookies) {
+            return subCookies[subName];
+        } else {
+            return null;
+        }
+    },
+
+    getAll: function (name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null,
+            cookieEnd,
+            subCookies,
+            i, len,     // add self
+            parts,
+            result = {};
+
+        if (cookieStart > -1) {
+            cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = document.cookie.substring(cookieStart + cookieName.length, cookieEnd);
+
+            if (cookieValue.length > 0) {
+                subCookies = cookieValue.split("&");
+
+                for (i = 0, len = subCookies.length; i < len; i++) {
+                    parts = subCookies[i].split("=");
+                    result[decodeURIComponent(parts[0])] =
+                        decodeURIComponent(parts[1]);
+                }
+
+                return result;
+            }
+        }
+
+        return null;
+    },
+
+    set: function (name, subName, value, expires, path, domain, secure) {
+        var subcookies = this.getAll(name) || {};
+        subcookies[subName] = value;
+        this.setAll(name, subcookies, expires, path, domain, secure);
+    },
+
+    setAll: function (name, subcookies, expires, path, domain, secure) {
+
+        var cookieText = encodeURIComponent(name) + "=",
+            subcookieParts = [],
+            subName;
+
+        for (subName in subcookies) {
+            if (subName.length > 0 && subcookies.hasOwnProperty(subName)) {
+                subcookieParts.push(encodeURIComponent(subName) + "=" +
+                    encodeURIComponent(subcookies[subName]));
+            }
+        }
+
+        if (subcookieParts.length > 0) {
+            cookieText += subcookieParts.join("&");
+
+            if (expires instanceof Date) {
+                cookieText += "; expires=" + expires.toGMTString();
+            }
+
+            if (path) {
+                cookieText += "; path=" + path;
+            }
+
+            if (domain) {
+                cookieText += "; domain=" + domain;
+            }
+
+            if (secure) {
+                cookieText += "; secure";
+            } 
+        } else {
+            cookieText += "; expires=" + (new Date(0)).toGMTString();
+        }
+
+        document.cookie = cookieText;
+    },
+
+    unset: function (name, subName, path, domain, secure) {
+        var subcookies = this.getAll(name);
+        if (subcookies) {
+            delete subcookies[subName];
+            this.setAll(name, subcookies, null, path, domain, secure);
+        }
+    },
+
+    unsetAll: function (name, path, domain, secure) {
+        this.setAll(name, null, new Date(0), path, domain, secure);
+    }
+};
+/* $end cookie ojection construction */
+
 
 function getHTTPObject() {
     if (XMLHttpRequest) {
@@ -57,7 +211,24 @@ function resetBlank(whichBlank) {
 /*
  * To submit form via ajax
  */
+
+/* $begin globel variable */
+//var labels = ["name", "email", "qq", "phone", "sug"];
+
+/* $end globel variable */
+
+
 function formSubmit() {
+    /* 
+     * IF has form submit cookie THEN return false
+     */
+    var formSubmitCookie = CookieUtil.get("form");
+    if (formSubmitCookie == "true") {
+        alert("You seem to submit once. Try again later.");
+        resetForm();
+        return false;
+    }
+
     var labels = ["name", "email", "qq", "phone", "sug"];
     var placeholder = ["Your Name", "Your Email", "Your QQ numbers", "Your Telephone", "Why you want to join us?"];
 
@@ -144,7 +315,14 @@ function formSubmit() {
     /* end FormData */
 
     var xhr = submitDataWithAjax("form.php", data);
-    if (xhr == 1) { alert("Commit successfully."); }
+    if (xhr == 1) {
+        alert("Commit successfully.");
+        /* set form-submit cookie */
+        CookieUtil.set("form", "true");
+
+    } else {
+        alert("Commit Failed. Try again, please.");
+    }
     resetForm();
 
     return false;
@@ -154,20 +332,28 @@ function formSubmit() {
  * To save like count via xml
  */
 function likeSubmit(name) {
+    /*
+     * IF cookie has likeClick history in 12Hs THEN return false
+     */
+    var likeClickCookie = SubCookieUtil.get("like", name);
+    if (likeClickCookie == "true") { return false;}
+
     var data = new FormData();
     data.append("name", name);
 
     var xhr = submitDataWithAjax("like.php", data);
     //More detail code write here
-    var targets = document.getElementsByClassName("likenumbers");
+    var targets = document.getElementsByClassName("like");
     var labels = ["csong", "sss", "pigeon", "wb"];
     var x, theTarget;
     for (x in targets) {
         if (labels[x] == name) {
-            theTarget = targets[x];
+            theTarget = targets[x].getElementsByClassName(".likenumbers")[0];
         }
     }
-    theTarget.innerHTML = xhr;
+    theTarget.innerHTML = "已赞(" + xhr + ")";
+    /* set like-click cookie */
+    SubCookieUtil.set("like", name, "true");
 
     return false;
 }
