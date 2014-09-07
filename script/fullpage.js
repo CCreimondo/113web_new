4fb5324cf0584eed78ac450b41bebbec9731c3b4
Boby <zsb0213@gmail.com>
2014-09-07 13:44:23 +0800

    improve load speed 1.0

--- script/fullpage.js
+++ script/fullpage.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
 
     $.fn.fullpage({
-        slidesColor: ['#1bbc9b', '#4BBFC3', '#FFFFFF', '#f90'],
+        slidesColor: ['#FFFFFF', '#A0D8EF', '#FFFFFF', '#f90'],
         anchors: ['page1', 'page2', 'page3', 'page4'],
         navigation: true,
         afterLoad: function (anchorLink, index) {
@@ -397,61 +397,61 @@
     */
     $(".nav_members_1").click(function () {
         $(".hp_1").addClass("test");
-        $(".hp_4").removeClass("test");
-        $(".hp_1").delay(799).animate({
+        $(".hp_3").removeClass("test");
+        $(".hp_1").delay(800).animate({
             opacity: '0'
         }, 0);
-        $(".hp_2").delay(799).animate({
+        $(".hp_2").delay(800).animate({
             opacity: '1'
         }, 0);
         $(".photos_1").css("z-index", "9");
         $(".photos_2").css("z-index", "10");
         $(".photos_3").css("z-index", "9");
-        $(".photos_4").css("z-index", "9");
+        //$(".photos_4").css("z-index", "9");
     });
 
     $(".nav_members_2").click(function () {
         $(".hp_2").addClass("test");
         $(".hp_1").removeClass("test");
-        $(".hp_2").delay(799).animate({
+        $(".hp_2").delay(800).animate({
             opacity: '0'
         }, 0);
-        $(".hp_3").delay(799).animate({
+        $(".hp_3").delay(800).animate({
             opacity: '1'
         }, 0);
         $(".photos_1").css("z-index", "9");
         $(".photos_2").css("z-index", "9");
         $(".photos_3").css("z-index", "10");
-        $(".photos_4").css("z-index", "9");
+        //$(".photos_4").css("z-index", "9");
     });
     $(".nav_members_3").click(function () {
         $(".hp_3").addClass("test");
         $(".hp_2").removeClass("test");
-        $(".hp_3").delay(799).animate({
+        $(".hp_3").delay(800).animate({
             opacity: '0'
         }, 0);
-        $(".hp_4").delay(799).animate({
+        $(".hp_1").delay(800).animate({
             opacity: '1'
         }, 0);
-        $(".photos_1").css("z-index", "9");
+        //$(".photos_1").css("z-index", "9");
         $(".photos_2").css("z-index", "9");
         $(".photos_3").css("z-index", "9");
-        $(".photos_4").css("z-index", "10");
-    });
-    $(".nav_members_4").click(function () {
-        $(".hp_4").addClass("test");
-        $(".hp_3").removeClass("test");
-        $(".hp_4").delay(799).animate({
-            opacity: '0'
-        }, 0);
-        $(".hp_1").delay(799).animate({
-            opacity: '1'
-        }, 0);
         $(".photos_1").css("z-index", "10");
-        $(".photos_2").css("z-index", "9");
-        $(".photos_3").css("z-index", "9");
-        $(".photos_4").css("z-index", "9");
     });
+    //$(".nav_members_4").click(function () {
+    //    $(".hp_4").addClass("test");
+    //    $(".hp_3").removeClass("test");
+    //    $(".hp_4").delay(799).animate({
+    //        opacity: '0'
+    //    }, 0);
+    //    $(".hp_1").delay(799).animate({
+    //        opacity: '1'
+    //    }, 0);
+    //    $(".photos_1").css("z-index", "10");
+    //    $(".photos_2").css("z-index", "9");
+    //    $(".photos_3").css("z-index", "9");
+    //    $(".photos_4").css("z-index", "9");
+    //});
     /*
 
     //淡出效果
