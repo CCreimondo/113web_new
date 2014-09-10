51e019f73bb7bb6ae12e5b10350e99949223b8a5
reimondo <reimondo.zero@gmail.com>
2014-09-10 19:02:45 +0800

    add get_client_ip function

---
+++ service.php
@@ -0,0 +1,22 @@
+﻿<?php
+	date_default_timezone_set('PRC');
+	
+	function get_client_ip() {
+		if(getenv('HTTP_CLIENT_IP')) {
+			$client_ip = getenv('HTTP_CLENT_IP');
+		} else if (getenv('HTTP_X_FORWARD_FOR')) {
+			$client_ip = getenv('HTTP_X_FORWARD_FOR');
+		} else if (getenv('REMOTE_ADDR')) {
+			$client_ip = getenv('REMOTE_ADDR');
+		} else {
+			$client_ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $client_ip;
+	}
+
+	if ( $ip = get_client_ip() ) {
+		echo $ip;
+	} else {
+		die();
+	}
+?>
